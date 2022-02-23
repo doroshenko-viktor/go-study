fc03e74442b167f0a50e51be93686eed1e8f87e5
Viktor Doroshenko <vdoroshenko@veepee.com>
2022-02-23 11:56:47 +0200

    fanout + turnout patterns

---
+++ concurrency/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Go concurrency examples")
+}
+
+// returns data from channel if it received during given period of time, else will return default value
+func TryReceiveWithTimeout(c <-chan int, duration time.Duration) (data int, more, ok bool) {
+	select {
+	case data, more = <-c:
+		return data, more, true
+	case <-time.After(duration):
+		return 0, true, false
+	}
+}
+
+// sends data from In to firts channel, which will not be blocked
+func Funout(In <-chan int, OutA, OutB chan<- int) {
+	for data := range In { // this will receive data from In untill it will be closed
+		select {
+		case OutA <- data:
+		case OutB <- data:
+		}
+	}
+}
+
+// receives data from first non blocked input channels and sends
+// it to the first not blocked output channel
+func Turnout(InA, InB <-chan int, OutA, OutB chan<- int) {
+	for {
+		select {
+		case data, more := <-InA:
+			if !more {
+				return
+			}
+			select {
+			case OutA <- data:
+			case OutB <- data:
+			}
+		case data, more := <-InB:
+			if !more {
+				return
+			}
+			select {
+			case OutA <- data:
+			case OutB <- data:
+			}
+		}
+	}
+}
